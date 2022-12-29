a4256cb3e08c4cef41713f9cfb98b4aebf7eedfe
boni <kgb8594@gmail.com>
2022-12-30 00:57:03 +0900

    회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,10 @@
 package hello.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -30,6 +33,14 @@ public class MemberController {
         service.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = service.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 
 }
